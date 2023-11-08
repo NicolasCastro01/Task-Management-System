e922cf7a8b9d4a075ea7f694ed84eedfc066d1e6
Nicolas Castro <nicolas.y0909@gmail.com>
2023-11-08 18:42:33 -0300

    feat(src/contracts/repositories/task/task.repository): implementando novos métodos no contrato.

--- src/contracts/repositories/task/task.repository.ts
+++ src/contracts/repositories/task/task.repository.ts
@@ -1,10 +1,13 @@
 import { Task } from "~/core/task";
 import { UpdatedTask } from "~/dtos/task/task";
+import { StatusEnum } from "~/enum/task/status";
 
 export interface TaskRepository {
     getAll(): Promise<Task[]>;
+    getAllByStatus(status: StatusEnum): Promise<Task[]>;
     findById(taskId: number): Promise<Task | undefined>;
     create(task: Task): Promise<void>;
     delete(task: Task): Promise<void>;
     update(updatedTask: UpdatedTask): Promise<void>;
+    complete(taskId: number): Promise<void>;
 }
