6624f0494c883c5a01162dd16ec1e0917554666b
Nicolas Castro <nicolas.y0909@gmail.com>
2023-11-15 13:41:05 -0300

    refactor(src/app, src/routes/task/task.route): remoção de duplicidade nas rotas das tarefas. remoção das tarefas iniciais no app.ts.

--- src/app.ts
+++ src/app.ts
@@ -6,21 +6,9 @@ const port: number = Number(process.env.PORT) || 3000;
 
 app.use(express.json());
 
-app.use('/', TaskRoute);
+app.use('/api/tasks', TaskRoute);
 
 app.listen(port, () => {
     console.clear();
     console.log('[⚙️] > Server on!');
 });
-
-// [x] - Criar uma nova tarefa com um título, uma descrição e uma data de vencimento.
-// [x] - Listar todas as tarefas.
-// [x] - Marcar uma tarefa como concluída.
-// [x] - Editar o título, descrição ou data de vencimento de uma tarefa.
-// [x] - Excluir uma tarefa.
-// [x] - Filtrar tarefas por status (concluídas ou pendentes).
-// [x] - Filtrar tarefas por data de vencimento.
-
-// [x] - Implementar o Prisma.
-// [x] - Banco de dados postgres.
-// [x] - Modelar a tarefa para o banco.
--- src/routes/task/task.route.ts
+++ src/routes/task/task.route.ts
@@ -13,11 +13,11 @@ const taskRepository = new TaskRepository(database);
 const taskService = new TaskService(taskRepository);
 const taskController = new TaskController(taskService);
 
-router.get('/api/tasks', async (request: Request, response: Response) => taskController.getAll(request, response));
-router.get('/api/tasks/list', async (request: Request, response: Response) => taskController.getAllByFilter(request, response));
-router.post('/api/tasks/create', async (request: Request, response: Response) => taskController.create(request, response));
-router.patch('/api/tasks/:taskId/edit', async (request: Request, response: Response) => taskController.update(request, response));
-router.patch('/api/tasks/:taskId/complete', async (request: Request, response: Response) => taskController.complete(request, response));
-router.delete('/api/tasks/:taskId/delete', async (request: Request, response: Response) => taskController.delete(request, response));
+router.get('/', async (request: Request, response: Response) => taskController.getAll(request, response));
+router.get('/list', async (request: Request, response: Response) => taskController.getAllByFilter(request, response));
+router.post('/create', async (request: Request, response: Response) => taskController.create(request, response));
+router.patch('/:taskId/edit', async (request: Request, response: Response) => taskController.update(request, response));
+router.patch('/:taskId/complete', async (request: Request, response: Response) => taskController.complete(request, response));
+router.delete('/:taskId/delete', async (request: Request, response: Response) => taskController.delete(request, response));
 
 export default router;
