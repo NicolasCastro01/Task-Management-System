b24887443faff58a2576039b57b3b71e5720d8e6
Nicolas Castro <nicolas.y0909@gmail.com>
2023-11-17 20:05:44 -0300

    feat(src/repositories/user/user.repository): criação do repository do usuário.

---
+++ src/repositories/user/user.repository.ts
@@ -0,0 +1,44 @@
+import { PrismaClient } from "@prisma/client";
+import { UserFromPrismaAdapter } from "~/adapters/prisma/user/userFromPrismaAdapter";
+import { UserRepository as UserRepositoryContract } from "~/contracts/repositories/user/user.repository";
+import { User } from "~/core/user";
+import { ResourceException } from "~/exception/ResourceException";
+
+export class UserRepository implements UserRepositoryContract {
+    constructor(
+        private readonly prismaClient: PrismaClient
+    ) { }
+
+    async findById(userId: number): Promise<User> {
+        const user = await this.prismaClient.user.findUnique({ where: { id: userId } });
+
+        if(!user) {
+            throw ResourceException.notFound({ field: 'user' });
+        }
+
+        return UserFromPrismaAdapter.convert(user);
+    }
+
+    async create(user: User): Promise<void> {
+        await this.prismaClient.user.create({
+            data: {
+                firstName: user._props.firstName,
+                lastName: user._props.lastName,
+                email: user._props.email,
+                password: user._props.password
+            }
+        });
+    }
+    
+    async findByEmail(email: string): Promise<User>{
+        const user = await this.prismaClient.user.findUnique({
+            where: { email }
+        });
+
+        if(!user) {
+            throw ResourceException.notFound({ field: 'user' });
+        }
+
+        return UserFromPrismaAdapter.convert(user);
+    }
+}
