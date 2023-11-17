b7ebf62d54a1167af0504fae4ab79fa975f12e21
Nicolas Castro <nicolas.y0909@gmail.com>
2023-11-17 20:10:17 -0300

    feat(src/app): utilização das rotas de autenticação e do middleware para a proteção das rotas de tarefas.

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,20 @@
 import 'express-async-errors';
 import express from "express";
 import TaskRoute from "~/routes/task/task.route";
+import AuthRoute from "~/routes/auth/auth.route";
 import { ErrorHandler } from "./middleware/error-handler/error-handler";
+import dotenv from "dotenv";
+import { AuthMiddleware } from './middleware/auth/auth.middleware';
+dotenv.config();
 
 const app: express.Application = express();
 const port: number = Number(process.env.PORT) || 3000;
 
 app.use(express.json());
 
+app.use('/api/auth', AuthRoute);
+
+app.use(AuthMiddleware.init);
 app.use('/api/tasks', TaskRoute);
 
 app.use(ErrorHandler.init);
@@ -19,5 +26,5 @@ app.listen(port, () => {
 
 // Authentication
 // [x] - Modelar usuário no prisma
-// [] - Criar sistema de cadastro
-// [] - Criar sistema de login
+// [x] - Criar sistema de cadastro
+// [x] - Criar sistema de login
