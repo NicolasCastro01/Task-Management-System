b0ea47776e6ae8ba5e780bafa8ce9541ff849363
Nicolas Castro <nicolas.y0909@gmail.com>
2023-11-22 20:20:56 -0300

    feat(src/adapters/prisma/user/userFromPrismaAdapter): mapeando as tarefas para o usuário.

--- src/adapters/prisma/user/userFromPrismaAdapter.ts
+++ src/adapters/prisma/user/userFromPrismaAdapter.ts
@@ -2,15 +2,20 @@ import {
     User as UserModel
  } from "@prisma/client";
 import { User } from "~/core/user";
+import { TaskFromPrismaAdapter, TaskMapper } from "../task/taskFromPrismaAdapter";
 
+type UserMapper = UserModel & {
+    Task: TaskMapper[]
+}
 
 export class UserFromPrismaAdapter {
-    static convert({ id, firstName, lastName, email, password }: UserModel): User {
+    static convert({ id, firstName, lastName, email, password, Task }: UserMapper): User {
         return User.restore({
             firstName,
             lastName,
             email,
-            password
+            password,
+            tasks: Task.map(TaskFromPrismaAdapter.convert)
         }, id);
     }
 }
